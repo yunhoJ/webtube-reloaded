2e1d828f854b3da4e21ecb6df1a453c1bda55b0b
unknown <wjse0213@naver.com>
2022-02-10 23:48:53 +0900

    build:first server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app=express();
 
-const port=9000;
-const listenServer=() =>console.log(`✅server listening on port http://localhost:${port}`)
-app.listen(port,listenServer)
+const PORT=9000;
+const listenServer=() =>
+    console.log(`✅server listening on port http://localhost:${PORT} `)
+app.listen(PORT,listenServer)
