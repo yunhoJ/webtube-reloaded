14be45271235a4d137078b9c021cd0ad306b6cb8
unknown <wjse0213@naver.com>
2022-02-11 00:33:45 +0900

    build:req,res사용하여 응답

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 const app=express();
-
 const PORT=9000;
+
+app.get("/",(requests,response)=>{
+    return response.send("i still love you")//텍스트 보내줌
+})
+app.get("/end",(requests,response)=>{
+    return response.end()//리퀘스트 종료
+})
 const listenServer=() =>
-    console.log(`✅server listening on port http://localhost:${PORT} `)
-app.listen(PORT,listenServer)
+    console.log(`✅server listening on port http://localhost:${PORT}  `);
+app.listen(PORT,listenServer);
