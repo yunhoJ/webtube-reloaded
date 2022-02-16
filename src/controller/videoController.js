d03ed0e3455409da9b9dd8bdd546625e5449dee5
unknown <wjse0213@naver.com>
2022-02-16 23:10:51 +0900

    build:pug 확장 - extends

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -2,14 +2,11 @@ export const trending = (req,res) => res.render("home");
 export const search =(req,res)=> res.send("video search")
 export const see = (req,res) =>{
     console.log(req.params);
-    return res.send(`see video #${req.params.id}`);
+    res.render("watch")
 }
 export const deleteVideo = (req,res)=> {
     console.log(req.params);
     return res.send("Delete video");
 }
 export const upload =(req,res) => res.send("upload video");
-export const edit=(req,res)=>{
-    console.log(req.params);
-    return res.send("edit Video");
-} 
+export const edit=(req,res)=>res.render("edit")
