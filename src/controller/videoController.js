6d18f3926e8998a647ccd576901b304b3d42a11c
unknown <wjse0213@naver.com>
2022-02-16 23:30:36 +0900

    build:pug변수 - render에서제공

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,12 @@
-export const trending = (req,res) => res.render("home");
+export const trending = (req,res) => res.render("home",{pageTitle:"Home"});
 export const search =(req,res)=> res.send("video search")
 export const see = (req,res) =>{
     console.log(req.params);
-    res.render("watch")
+    res.render("watch",{pageTitle:"Watch"})
 }
 export const deleteVideo = (req,res)=> {
     console.log(req.params);
     return res.send("Delete video");
 }
 export const upload =(req,res) => res.send("upload video");
-export const edit=(req,res)=>res.render("edit")
+export const edit=(req,res)=>res.render("edit",{pageTitle:"Edit"})
